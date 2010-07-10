9822eeb21774dc960975e5a9f512b5692994bb03
Martin Pärtel <martin.partel@gmail.com>
2010-07-10 12:16:03 +0300

    added option in settings for via field

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -7,13 +7,14 @@ class SettingsController < ApplicationController
   def update
     user = current_user
     
-    settings_to_save = ['dont_require_login']
+    settings_to_save = ['dont_require_login', 'show_via_field']
     settings_to_save.each do |key|
       setting = user.get_setting(key)
       setting.value = if params[:settings] then params[:settings][key] else nil end
       setting.save!
     end
 
+    flash[:success] = t('settings.saved')
     redirect_to settings_path
   end
 
--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -25,24 +25,40 @@ describe SettingsController do
   describe "#update" do
     before do
       @dont_require_login = mock_model(Setting)
+      @show_via_field = mock_model(Setting)
       controller.stub_chain(:current_user, :get_setting).with('dont_require_login').and_return(@dont_require_login)
+      controller.stub_chain(:current_user, :get_setting).with('show_via_field').and_return(@show_via_field)
     end
 
     describe "given new settings" do
       before do
         @params = {
           'settings' => {
-            'dont_require_login' => '1'
+            'dont_require_login' => '1',
+            'show_via_field' => '0'
           }
         }
 
         @dont_require_login.should_receive(:value=).with('1')
         @dont_require_login.stub!(:save!)
+
+        @show_via_field.should_receive(:value=).with('0')
+        @show_via_field.stub!(:save!)
       end
 
       it "should save dont_require_login" do
         @dont_require_login.should_receive(:save!)
         put_update
+      end
+
+      it "should save show_via_field" do
+        @show_via_field.should_receive(:save!)
+        put_update
+      end
+
+      it "should set a success message" do
+        put_update
+        flash[:success].should_not be_empty
       end
 
       it "should redirect to the show settings page" do
@@ -52,9 +68,11 @@ describe SettingsController do
     end
 
     describe "given no parameters" do # (all parameters nil)
-      it "should unset dont_require_login" do
+      it "should unset all parameters" do
         @dont_require_login.should_receive(:value=).with(nil)
         @dont_require_login.should_receive(:save!)
+        @show_via_field.should_receive(:value=).with(nil)
+        @show_via_field.should_receive(:save!)
         put_update
       end
     end
