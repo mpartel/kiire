6e70b2ee099ad8a2a21aa5d82e2fa8f6a13b1dc5
Martin Pärtel <martin.partel@gmail.com>
2011-04-15 13:32:36 +0300

    fixed cucumber tests

--- features/step_definitions/session.rb
+++ features/step_definitions/session.rb
@@ -3,19 +3,18 @@ Given /^I have no active session$/ do
 end
 
 Given /^I have logged in(?: as "([^"]*)")?$/ do |username|
-
-  @my_user ||= Factory.create(:user)
+  @my_user ||= Factory.build(:user)
   @my_user.username = username if username
   @my_user.password = 'furniture'
   @my_user.password_confirmation = 'furniture'
-  @my_user.save
-
+  @my_user.save!
+  
   visit path_to("the homepage")
-  response.should contain("Username")
+  page.should have_content("Username")
   fill_in "Username", :with => @my_user.username
   fill_in "Password", :with => @my_user.password
   click_button "Log in"
-  response.should contain("Log out")
+  page.should have_content("Log out")
 end
 
 When /^I fill in my username$/ do
@@ -24,9 +23,9 @@ When /^I fill in my username$/ do
 end
 
 When /^I fill in my password$/ do
-  @my_user ||= Factory.create(:user)
+  @my_user ||= Factory.build(:user)
   @my_user.password = 'furniture'
   @my_user.password_confirmation = 'furniture'
-  @my_user.save
+  @my_user.save!
   fill_in("Password", :with => @my_user.password)
 end
