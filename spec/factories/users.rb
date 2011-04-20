da6f3ad651cf16f6e57afbb8b1904bf042fac80f
Martin Pärtel <martin.partel@gmail.com>
2011-04-20 13:05:06 +0300

    minor

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,10 +1,4 @@
-Factory.sequence :username do |n|
-  good_guys = ['mal', 'zoe', 'wash', 'jayne', 'inara', 'kaylee', 'river', 'simon', 'book']
-  name = good_guys[n % good_guys.length]
-  round = n / good_guys.length
-  name += '_' + round.to_s unless round == 0
-  name
-end
+Factory.sequence(:username) { |n| "user#{n}" }
 
 Factory.define(:user) do |u|
   u.username { Factory.next(:username) }
