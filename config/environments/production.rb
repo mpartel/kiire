53acab3a5549f4b9b1a3112b52c03e09cd8244ff
Martin Pärtel <martin.partel@gmail.com>
2012-07-26 15:33:50 +0300

    Precompile all JS files in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ Kiire::Application.configure do
 
   # Precompile additional assets (common.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( common.js kiire.js settings.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
